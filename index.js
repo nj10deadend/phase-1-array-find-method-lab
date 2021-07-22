adaeadd137af227cbbdffedfd4ff1609bf298cc2
nj10deadend <nasjacks10@gmail.com>
2021-07-22 18:58:08 -0400

    commit tree 7ff03ffee63363559907ce8daf9c41babce9d3be

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    const foundSeason = record.find(season => season.result === "W")
+        if(!foundSeason) {
+            return undefined
+        } else {
+            return foundSeason.year
+        }
+}
